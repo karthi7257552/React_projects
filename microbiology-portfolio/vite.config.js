9e19d90b7fdeea2b8cf83ac0215bad81d8eaa44e
karthi7257552 <karthikkarthi7257552@gmail.com>
2025-07-24 16:48:02 +0530

    `Added gh-pages and vite-plugin-gh-pages dependencies, updated package-lock.json and package.json, and modified vite.config.js to use a custom base path.`

--- microbiology-portfolio/vite.config.js
+++ microbiology-portfolio/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Replace with your GitHub repo name
 export default defineConfig({
+  base: 'React_projects',
   plugins: [react()],
 })
